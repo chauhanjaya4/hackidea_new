c6f8dfc976f1f009410ae3c63596c08dc65d062c
chauhanjaya4 <chauhanjaya01@gmail.com>
2022-03-05 17:56:32 +0530

    worked on admin.py

--- django_project/theblog/models.py
+++ django_project/theblog/models.py
@@ -7,9 +7,10 @@ class BlogPost(models.Model):
     slug = models.SlugField()
     category = models.CharField(max_length=50, choices=Categories.choices, default=Categories.WORLD)
 thumbnail = models.ImageField(upload_to='photos/%Y/%m/%d/')
-excerpt = models.CharField(max_length=150)    
+excerpt = models.CharField(max_length=150)   
+month = models.CharField(max_length=3) 
+day = models.CharField(max_length=2)
+content = models.TextField
     
     #field_name = models.ImageField(blank=True,upload_to=None, height_field=None, width_field=None, max_length=100)
 
-def __str__(self):
-    return self.title 
